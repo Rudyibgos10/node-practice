855fa10f3822f5c02c1eaa5e3d327f23e1627fef
rudyni <gt201902659@wmsu.edu.ph>
2026-01-13 12:19:58 -0500

    JSON & APIs

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 console.log("Hi");
 
 console.log("Understood the process of creating the package.json");
+console.log("recalled on conditional on checking APIs and method— 1/13/26 EST")
+
+// changed the start script in package.json
+// changed again the start script in package.json to index
+
+// lesson continuation—1/13/26 JSON and APIs
+
+// creating object and convert it to JSON using JSON.stringify() function for example
+
+const animal = {
+  type: 'cattle',
+  name: 'Yak Nicholson'
+}
+
+console.log(typeof JSON.stringify(animal));
--- server.js
+++ server.js
@@ -15,12 +15,16 @@ const PORT = 8000;
 
 // accessing http method
 // creating the first server
-// (req, res => {res.}) is a callback function
-const server = http.createServer((req, res) => {
+// ((req, res) => {res.}) is a callback function
+
+/*
+const server = http.createServer((req, res) => { —commented this for next lesson
   // res.write('Hello from the server!') could use this
   // res.ed()
   res.end('Hello from the server!')
 });
+*/
+
 /* The response object— has methods which allow us to:
 —specify content-type
 —set status codes (200, 400 etc.)
@@ -30,4 +34,30 @@ const server = http.createServer((req, res) => {
 
 // reminder to connect to server first before sending a request — npm start or node server.js
 // listen method
+
+//server.listen(PORT, () => {console.log(`server running on port: ${PORT}`)});—commented this for next lesson
+
+
+
+// ---------------------------------------------------------------------------------->
+// Lesson continuation 1/13/26
+
+/*
+The request Object—gives us access to the incoming object
+—the URL the client used
+—the headers
+—any data sent
+—the method (GET, POST, DELETE, ETC.)
+*/
+
+
+const server = http.createServer((req, res) => {
+
+  // added a condition for checking the url equals to api and method equals to 'GET'
+  if (req.url === '/api' && req.method === 'GET') {
+    res.end('Hello from the server!')
+  }
+  // did try to run this on the server, got actually connected to the server but, I dont know yet how use third party apps or website to the route or api—will be practicing postman application soon so that I test the APIs i'm practicing.
+});
+
 server.listen(PORT, () => {console.log(`server running on port: ${PORT}`)});
